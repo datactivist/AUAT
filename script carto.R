1bdaa687c897036760ca3416303b85e72bbcc759
Joel Gombin <joel.gombin@gmail.com>
2017-10-19 15:45:43 +0200

    script carto

---
+++ script carto.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(sf)
+library(mapview)
+
+elections <- read_csv("./data/Presidentielle_2017_Resultats_Communes_T1_clean.csv")
+
+communes <- read_sf("./data/communes-20150101-100m.shp", stringsAsFactors = FALSE)
+
+elections <- communes %>% 
+  left_join(elections, by = c("insee" = "CodeInsee"))
+
+
+elections_dpt <- elections %>% 
+  group_by(CodeDepartement) %>% 
+  select(contains("_ins"), Inscrits) %>% 
+  summarise_at(vars(contains("_ins")), funs(weighted.mean(., w = Inscrits, na.rm = TRUE)))
+
+inscrits <- elections %>% 
+  group_by(CodeDepartement) %>% 
+  summarise(Inscrits = sum(Inscrits))
+
+departements <- communes %>% 
+  mutate(codedpt = substr(insee, 1, 2)) %>%
+  group_by(codedpt) %>% 
+  summarise()
+  
+elections_dpt <- departements %>% 
+  left_join(as.data.frame(elections_dpt) %>% select(-geometry), by = c("codedpt" = "codedpt")) %>% 
+  left_join(as.data.frame(inscrits) %>% select(-geometry), by = c("codedpt" = "CodeDepartement"))
+
+library(tmap)
+
+
+
+carte <- elections_dpt %>% 
+  filter(codedpt < 96) %>% 
+  tm_shape() +
+  tm_polygons() +
+#    tm_polygons(col = "Abstentions_ins", palette = "PuRd", n = 7, style = "jenks", title = "Abstention", textNA = "valeur manquante", legend.format = list(text.separator = "à")) +
+#    tm_text(text = "codedpt", size = 0.8) +
+    tm_bubbles(size = "Inscrits", col = "Abstentions_ins", scale = 2, palette = "PuRd", n = 7, style = "jenks", textNA = "valeur manquante", legend.format = list(text.separator = "à")) + 
+    tm_compass(position = c("right", "top")) +
+    tm_scale_bar(position = c("center", "bottom")) +
+  tm_layout(title = "Ma première carte")
+
+tmap_leaflet(carte)
+
+
+# avec cartography
+
+library(cartography)
+elections_dpt %>% 
+  filter(codedpt < 96) %>%
+  choroLayer(var = "Abstentions_ins", method = "quantile", nclass = 7)
