c81a70dd4c90c04da567738fcad4ad599f21a9bc
Endi Pons <iroitosheik@users.noreply.github.com>
2017-10-19 10:15:34 +0200

    Update osmosis.sh
    Problème MDP

--- osmosis.sh
+++ osmosis.sh
@@ -1,1 +1,1 @@
-osmosis --read-pbf file='/media/Data/Dropbox/datactivi.st/AU Toulouse/Formation AUTA/data/ex_dSohv2TLDASLFjBXiW5Z3rZqWSNwD.osm.pbf' --write-pgsql host="localhost" database="toulouse" user="joel" password="hirchberg"
+osmosis --read-pbf file='/media/Data/Dropbox/datactivi.st/AU Toulouse/Formation AUTA/data/ex_dSohv2TLDASLFjBXiW5Z3rZqWSNwD.osm.pbf' --write-pgsql host="localhost" database="toulouse" user="joel" password="*********"
